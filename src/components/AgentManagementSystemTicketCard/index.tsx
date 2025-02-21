8a15e1908983eedabb115a86dda9b8d927f8c5ca
Sunshine160265 <crown160265@gmail.com>
2025-02-21 10:15:18 -0800

    Some changes in AgentManagementSystemTicketCard

--- src/components/AgentManagementSystemTicketCard/index.tsx
+++ src/components/AgentManagementSystemTicketCard/index.tsx
@@ -27,14 +27,7 @@ const AgentManagementSystemTicketCard: React.FC<Props> = ({data}) => {
           width: '280px',
           minHeight: '320px',
         }}>
-          <Box
-            sx={{
-              display: 'flex',
-              marginBottom: '25px',
-            }}
-          >
-    
-          </Box>
+         
           <Typography
             sx={{
               marginBottom: '25px',
